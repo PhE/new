822f0b2dea6e41fb00288aae5a6b9633a8f55622
Philippe ENTZMANN <philippe.entzmann@gmail.com>
2012-09-03 20:04:12 +0200

    image slider

--- web2py/applications/snapcop2_web2py/controllers/thumbnail.py
+++ web2py/applications/snapcop2_web2py/controllers/thumbnail.py
@@ -1,11 +1,27 @@
 import redis
 
 def thumb():
+    """
+    Renvoie dans photos la liste des x dernières photos prises
+  
+    """
+    # connexion redis
+    # récupérer le nb de photo prises (GET frames)
+    # récupérere la liste des chemins des photos (LRANGE -5 -1)
+
     cx=redis.Redis()
     frames = int(cx.get('frames'))
+    frames = 3
     image=[]
-    serie = cx.lrange('captures', -frames, -1)
-    
+    photos = cx.lrange('captures', -frames, -1)
+    photos = [
+      '/snapcop2_web2py/static/images/css3buttons_backgrounds.png', 
+      '/snapcop2_web2py/static/images/poweredby.png', 
+      '/snapcop2_web2py/static/images/css3buttons_backgrounds.png', 
+      '/snapcop2_web2py/static/images/ui-icons_222222_256x240.png', 
+    ]
+
+    ''' INUTILE !!!
     for i in range(0, frames):
         photo = serie[i]
         image+=[
@@ -17,6 +33,9 @@ def thumb():
         A(IMG(_src=URL('static/Photos', 'thumb%s'%stack), _alt="Capt"),
                   _href=URL('static/Photos', stack)),
               ]
-    return dict(Photos=image, Stacking=image2)
+    #return dict(Photos=image, Stacking=image2)
+    '''    
+    auteur = 'Eric DURAND'
+    return locals()
     
     
